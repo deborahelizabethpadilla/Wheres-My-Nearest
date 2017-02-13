7f72c9316da09e3673e86d79f3c398614629c473
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-12 20:43:31 -0500

    Changes

--- WheresMyNearest/ViewController.swift
+++ WheresMyNearest/ViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var defaults = UserDefaults(suiteName: "group.com.Deborah.wheresMyNearest")
+    
     @IBOutlet var place1: UITextField!
     @IBOutlet var place2: UITextField!
     @IBOutlet var place3: UITextField!
     @IBOutlet var place4: UITextField!
     
     @IBAction func update(_ sender: Any) {
+        place1.text
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
+        
+        if let storedPlacesArray = storedPlaces as? NSArray {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
