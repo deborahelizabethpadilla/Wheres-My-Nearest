f5921eb661738a0667ce7d3ddc88bed462d594a7
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-12 23:08:32 -0500

    Changes

---
+++ WheresMyNearest WatchKit App/mapInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  mapInterfaceController.swift
+//  WheresMyNearest
+//
+//  Created by Deborah on 2/12/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class mapInterfaceController: WKInterfaceController {
+
+}
