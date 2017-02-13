26a663a6598b542092e2a9fbf42d174874fb1d05
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-12 21:54:05 -0500

    Interface

--- WheresMyNearest WatchKit Extension/InterfaceController.swift
+++ WheresMyNearest WatchKit Extension/InterfaceController.swift
@@ -11,11 +11,34 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
+    
+    var places = [String]()
+    
+    var defaults = UserDefaults(suiteName: "group.com.Deborah.wheresMyNearest")
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        let storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
+        
+        if let storedPlacesArray = storedPlaces as? NSArray {
+        for (index, value) in storedPlacesArray.enumerated() {
+                
+        if let placeName = value as? String {
+        places.append(placeName)
+                    
+    }
+                
+    }
+            
+    }
+        
+    if places.count == 0 {
+    places = ["cafe", "cinema", "library", "pub"]
+    defaults?.set(places, forKey: "places")
+    }
+
+    print(places)
     }
     
     override func willActivate() {
@@ -28,4 +51,4 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
-}
+    }
