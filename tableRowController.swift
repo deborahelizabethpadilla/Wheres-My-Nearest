7ce8d2b92331da16dd7c8409aa1c563aca877b3b
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-12 22:25:12 -0500

    Edits

---
+++ tableRowController.swift
@@ -0,0 +1,13 @@
+//
+//  tableRowController.swift
+//  WheresMyNearest
+//
+//  Created by Deborah on 2/12/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class tableRowController: NSObject {
+
+}
