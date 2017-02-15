ddb74f45728675466d23b92dc42c40862da7a2f0
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-14 23:42:49 -0500

    Map Interface

--- WheresMyNearest WatchKit App/mapInterfaceController.swift
+++ WheresMyNearest WatchKit App/mapInterfaceController.swift
@@ -10,5 +10,48 @@ import UIKit
 import WatchKit
 
 class mapInterfaceController: WKInterfaceController {
-
+    
+    override func willActivate() {
+        let url = NSURL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyDVQdPf1UBH6sbLmCtsRWoRIgsouboYeRo")
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            if error == nil {
+                var jsonResult: NSDictionary = JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions) as NSDictionary
+                
+                print(jsonResult)
+                
+                let returnedPlaces: NSArray? = jsonResult
+                
+                if returnedPlaces != nil {
+                    if let returnedPlace = returnedPlaces?[0] as? NSDictionary {
+                        
+                        if let geometry = returnedPlace["geometry"] as? NSDictionary {
+                            
+                            if let name = returnedPlace["name"] as? NSString {
+                                
+                                print(name)
+                            }
+                            
+                            if let location = geometry["location"] as? NSDictionary {
+                                
+                                if let lat = location["lat"] as? Double {
+                                    
+                                    print(lat)
+                                }
+                                if let lng = location["lng"] as? Double {
+                                    
+                                    print(lng)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+            } else {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
 }
--- WheresMyNearest WatchKit Extension/InterfaceController.swift
+++ WheresMyNearest WatchKit Extension/InterfaceController.swift
@@ -16,76 +16,82 @@ class InterfaceController: WKInterfaceController {
     var places = [String]()
     
     var defaults = UserDefaults(suiteName: "group.com.Deborah.wheresMyNearest")
-
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-
-    let url = NSURL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyDVQdPf1UBH6sbLmCtsRWoRIgsouboYeRo")
-    let task = URLSession.shared.dataTask(with: url) {
-    (data, response, error) in
-    if error == nil {
-    var jsonResult: NSDictionary = JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions) as NSDictionary
         
-    print(jsonResult)
-        
-    let returnedPlaces: NSArray? = jsonResult
-        
-    if returnedPlaces != nil {
-        if let returnedPlace = returnedPlaces?[0] as? NSDictionary {
-            if let geometry = returnedPlace["geometry"] as? NSDictionary {
-                if let name = returnedPlace["name"] as? NSString {
-                    print(name)
+        let url = NSURL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyDVQdPf1UBH6sbLmCtsRWoRIgsouboYeRo")
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            if error == nil {
+                var jsonResult: NSDictionary = JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions) as NSDictionary
+                
+                print(jsonResult)
+                
+                let returnedPlaces: NSArray? = jsonResult
+                
+                if returnedPlaces != nil {
+                    if let returnedPlace = returnedPlaces?[0] as? NSDictionary {
+                        
+                        if let geometry = returnedPlace["geometry"] as? NSDictionary {
+                            
+                            if let name = returnedPlace["name"] as? NSString {
+                                
+                                print(name)
+                            }
+                            
+                            if let location = geometry["location"] as? NSDictionary {
+                                
+                                if let lat = location["lat"] as? Double {
+                                    
+                                    print(lat)
+                                }
+                                if let lng = location["lng"] as? Double {
+                                    
+                                    print(lng)
+                                }
+                            }
+                        }
+                    }
                 }
                 
-                if let location = geometry["location"] as? NSDictionary {
-                    if let lat = location["lat"] as? Double {
-                        print(lat)
-                    }
-                    if let lng = location["lng"] as? Double {
-                        print(lng)
-                    }
-                }
+            } else {
+                print(error)
             }
         }
-    }
-        
-    } else {
-    print(error)
-    }
-    }
-    task.resume()
+        task.resume()
     }
     
     override func willActivate() {
-    super.willActivate()
+        super.willActivate()
         
-    let storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
+        let storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
         
-    if let storedPlacesArray = storedPlaces as? NSArray {
-    for (index, value) in storedPlacesArray.enumerated() {
+        if let storedPlacesArray = storedPlaces as? NSArray {
+            for (index, value) in storedPlacesArray.enumerated() {
                 
-    if let placeName = value as? String {
-    places.append(placeName)
+                if let placeName = value as? String {
+                    places.append(placeName)
                     
-    }
+                }
                 
-    }
+            }
             
-    }
+        }
         
-    if places.count == 0 {
-    places = ["cafe", "cinema", "library", "pub"]
-    defaults?.set(places, forKey: "places")
-    }
-    
-    table.setNumberOfRows(places.count, withRowType: "tableRowController")
+        if places.count == 0 {
+            places = ["cafe", "cinema", "library", "pub"]
+            defaults?.set(places, forKey: "places")
+        }
         
-    for (index, place) in places.enumerated() {
-    let row = table.rowController(at: index) as! tableRowController
-    row.tableRowLabel.setText(place)
-    
+        table.setNumberOfRows(places.count, withRowType: "tableRowController")
+        
+        for (index, place) in places.enumerated() {
+            let row = table.rowController(at: index) as! tableRowController
+            row.tableRowLabel.setText(place)
             
-    }
+            
+        }
         
     }
     
