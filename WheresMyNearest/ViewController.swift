261abe207fd376bbe6fef84fb94d8327decc854e
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-02-18 00:37:45 -0500

    Core Location Functions

--- WheresMyNearest/ViewController.swift
+++ WheresMyNearest/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
     
     var places = [String]()
     
@@ -21,42 +23,51 @@ class ViewController: UIViewController {
     @IBOutlet var place4: UITextField!
     
     @IBAction func update(_ sender: Any) {
-    places = [place1.text!, place2.text!, place3.text!, place4.text!]
-    defaults?.set(places, forKey: "places")
+        places = [place1.text!, place2.text!, place3.text!, place4.text!]
+        defaults?.set(places, forKey: "places")
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-    let storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
-    if let storedPlacesArray = storedPlaces as? NSArray {
-    for (index, value) in storedPlacesArray.enumerated() {
+        let storedPlaces: AnyObject? = defaults?.object(forKey: "places") as AnyObject?
+        
+        if let storedPlacesArray = storedPlaces as? NSArray {
+            for (index, value) in storedPlacesArray.enumerated() {
                 
-    if let placeName = value as? String {
-    places.append(placeName)
+                if let placeName = value as? String {
+                    places.append(placeName)
                     
-    }
+                }
                 
+            }
+            
+        }
+        if places.count == 0 {
+            places = ["cafe", "cinema", "library", "pub"]
+            defaults?.set(places, forKey: "places")
+        }
+        place1.text = places[0]
+        place2.text = places[1]
+        place3.text = places[2]
+        place4.text = places[3]
     }
-        
-    }
-    if places.count == 0 {
-    places = ["cafe", "cinema", "library", "pub"]
-    defaults?.set(places, forKey: "places")
-    }
-    place1.text = places[0]
-    place2.text = places[1]
-    place3.text = places[2]
-    place4.text = places[3]
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
